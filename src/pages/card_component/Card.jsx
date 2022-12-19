9edb04bb63c1c6cf02306f3cb70ae8f676271658
Mittal <mittalkumargupta@gmail.com>
2022-12-19 20:16:05 +0530

    last commit mittal

--- src/pages/card_component/Card.jsx
+++ src/pages/card_component/Card.jsx
@@ -3,23 +3,27 @@ import card from './card.module.css'
 
 
 
-const Card = () => {
-
+const Card = ({ la,ti, di, din,g,l,s}) =>{
+   
 
     return (
         <>
             <div className={card.main}>
 
                 <div className={card.fram}>
-                    <div className={card.lang}>
-                       <h4>React</h4>
+                    <div className={la==="React"?card.lang:card.lang2}>
+                       <div className={card.h34}>
+                       <div className={card.ic1}><a className={card.go1} target="_blank" href={g}><i className="fa-brands fa-github"></i></a></div>
+                       <div className={card.ic2}><a className={card.go} target="_blank" href={l}><i className="fa-solid fa-link"></i></a></div>
+                       </div>
                     </div>
                     <div className={card.disc}>
-                      <div className={card.tital}><h3>weather app</h3></div>
-                      <div className={card.discription}><p className={card.d}>This is a dynamic weather updatyte website</p></div>
-                      <div className={card.date}><p className={card.dat} >12/06/2022-to-14/06/2022</p></div>
+                      <div className={card.tital}><h3 className={card.run1}>{ti}</h3> <div className={s!==""?(s==="d"?card.done:card.working):card.incomplet}></div></div>
+                      <div className={card.discription}><p className={card.d}>{di}</p></div>
+                      <div className={card.date}><p className={card.dat} >{din}</p></div>
                     </div>
                 </div>
+                
 
             </div>
         </>
--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,28 +1,44 @@
 import pro from './Project.module.css';
 import React from 'react'
+import axios from 'axios';
 import Card from '../card_component/Card';
-
+import { useState } from 'react';
+  
 const Project = () => {
+ //const val={lan:"mittal",tital:"Weather app",dic:"This is a dynamic weather app this application show current weather candation of any place as well as its show your current loction","datee":"12/12/2022-15/12/2022"};
+
+  const [arry,setar]=useState([]);
+
+ // const arry=[{lan:"React",tital:"Weather app",dic:"This is a dynamic weather app this application show current weather candation of any place as well as its show your current loction",datee:"12/12/2022-15/12/2022"},{lan:"Html,css,js",tital:"Otp verification ui",dic:"This is a simpale otp verification user interface",datee:"29/11/2022-30/11/2022"},]
+   /*  function po()
+     {
+       axios("http://localhost:8080/posts",val);
+     }
+     po();
+     */
+
+   const data=axios("http://localhost:8080/posts");
+   const download= async()=>{
+          const data1= await data;
+          setar(data1.data);
+   }
+
+   download();
+
 
   return (
     <div className={pro.main}>
-  
-      <Card/>
+
+   {
+       
+      arry.map((e,i)=>{
+        return <Card key={i} la={e.lan} ti={e.tital} di={e.dic} din={e.datee} g={e.git} l={e.link} s={e.status}/>
+      })
+
+   }
       
-      <Card/>
-      <Card/>
-      <Card/>
       
-      <Card/>
-      <Card/>
-      <Card/>
-      
-      <Card/>
-      <Card/>
-      <Card/>
-      
-      <Card/>
-      <Card/>
+    
 
 
     </div>
